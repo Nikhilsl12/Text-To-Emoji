cede25fc2b8af98e3bf78b051a0c2d82e2849c8e
Nikhil Singhal <Nikhilsinghal476@gmail.com>
2025-07-08 23:43:36 +0530

    external api check

--- src/main/java/org/coderscrib/texttoemojiwithsentiment/controller/EmojiController.java
+++ src/main/java/org/coderscrib/texttoemojiwithsentiment/controller/EmojiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// This controller is for external frontend
+// This controller is for the external frontend
 @RestController
 @RequestMapping("/api/emoji")
 public class EmojiController {
@@ -15,12 +15,12 @@ public class EmojiController {
     public EmojiController(EmojiService emojiService) {
         this.emojiService = emojiService;
     }
-
+    // this will be used for replacing some words in text to emoji
     @PostMapping("/replace")
     public String replaceText(@RequestBody String input) {
         return emojiService.replaceWithEmojis(input);
     }
-
+    // It helps you to get the sentiment score of the sentence.
     @PostMapping("/score")
     public String getSentimentScore(@RequestBody String input) {
         int score = emojiService.calculateSentimentScore(input);
